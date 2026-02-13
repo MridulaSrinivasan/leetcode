5ea8631d1b04e5fa701b0bcf56acdef2c2273261
MridulaSrinivasan <mridulasrinivasan05@gmail.com>
2026-02-13 11:32:20 +0530

    Create 2416-EvaluateBooleanBinaryTree

---
+++ 2416-EvaluateBooleanBinaryTree/2416-EvaluateBooleanBinaryTree.java
@@ -0,0 +1,32 @@
+// Last updated: 2/13/2026, 11:32:20 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {      
+    if (root.left == null && root.right == null) {
+            return root.val == 1;
+        }
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
+        if (root.val == 2) {
+            return left || right;  // OR
+        } else if (root.val == 3) {
+            return left && right; // AND
+        }
+        throw new IllegalArgumentException("Invalid non-leaf node value: " + root.val);    
+       
+    }
+}
