4385db16f3810e60c5f9197c5f7cbcfc351180f2
MridulaSrinivasan <mridulasrinivasan05@gmail.com>
2026-02-13 11:32:38 +0530

    Create 1035-CousinsInBinaryTree

---
+++ 1035-CousinsInBinaryTree/1035-CousinsInBinaryTree.java
@@ -0,0 +1,38 @@
+// Last updated: 2/13/2026, 11:32:37 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode xParent = null, yParent = null;
+    int xDepth = -1, yDepth = -1;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        dfs(root, null, 0, x, y);
+        return xDepth == yDepth && xParent != yParent;
+    }
+    private void dfs(TreeNode node, TreeNode parent, int depth, int x, int y) {
+        if (node == null) return;
+
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
+        } else if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+        }
+        dfs(node.left, node, depth + 1, x, y);
+        dfs(node.right, node, depth + 1, x, y);
+    }
+}
+
