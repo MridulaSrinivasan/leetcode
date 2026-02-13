eb3ad895e0d21974a7267169866017ead2b14100
MridulaSrinivasan <mridulasrinivasan05@gmail.com>
2026-02-13 11:32:21 +0530

    Create 2384-RootEqualsSumOfChildren

---
+++ 2384-RootEqualsSumOfChildren/2384-RootEqualsSumOfChildren.java
@@ -0,0 +1,26 @@
+// Last updated: 2/13/2026, 11:32:21 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        int a = root.left.val + root.right .val;
+        if(root.val == a){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
