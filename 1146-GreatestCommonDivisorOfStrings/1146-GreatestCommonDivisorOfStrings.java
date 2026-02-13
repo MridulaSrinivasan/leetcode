a2b0c5139c128add7846f06658e32083e8a87ea4
MridulaSrinivasan <mridulasrinivasan05@gmail.com>
2026-02-13 11:32:36 +0530

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,19 @@
+// Last updated: 2/13/2026, 11:32:36 AM
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int g = gcd(str1.length(), str2.length());
+        return str1.substring(0, g);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
